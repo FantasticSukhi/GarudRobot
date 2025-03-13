c6ebf5c5aa8bfa54855cbbf4a62cc8cd83317e84
Blackmamba <122316561+FantasticSukhi@users.noreply.github.com>
2025-03-13 22:57:27 +0530

    Update config.py

--- MukeshRobot/config.py
+++ MukeshRobot/config.py
@@ -1,20 +1,20 @@
 class Config(object):
     LOGGER = True
-    API_ID =None 
-    API_HASH = ""
-    TOKEN = ""  
-    OWNER_ID=None
+    API_ID =20539290 
+    API_HASH = "26e0c15a6803997a3e58d564be36f8f3"
+    TOKEN = "6821828314:AAES-su9N9iQrQ4LtD5BWClzJX2jfdfxkns"  
+    OWNER_ID=7448520005
     
-    SUPPORT_CHAT = "" 
-    START_IMG = ""
-    EVENT_LOGS = ()
-    MONGO_DB_URI= ""
+    SUPPORT_CHAT = "-1002124344872" 
+    START_IMG = "https://vault.pictures/p/08641f944b7b45ee90634dc44e2f85d4"
+    EVENT_LOGS =-1002124344872
+    MONGO_DB_URI= "mongodb://mongo:SKfqQfOojkSyZmLRhxufNMjbyQPgWTFW@nozomi.proxy.rlwy.net:52328"
    
-    DATABASE_URL = ""  # A sql database url from elephantsql.com
+    DATABASE_URL = "postgresql://neondb_owner:npg_FueRAIz48ZaS@ep-solitary-haze-a8lylhhz-pooler.eastus2.azure.neon.tech/neondb?sslmode=require"  # A sql database url from elephantsql.com
     CASH_API_KEY = (
-        ""
+        "2ATSJ753CLE402MQ"
     )
-    TIME_API_KEY = ""
+    TIME_API_KEY = ""9LMMJQ30GM49
 
     
     BL_CHATS = [] 
